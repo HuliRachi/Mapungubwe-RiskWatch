be4e4f1f071f6d4aaaedf349e2984360bd420ea4
hulisani <rachyhuly17@gmail.com>
2026-06-18 15:40:25 +0200

    fix: fixed composer bucket

--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -13,7 +13,7 @@ else:
 PROJECT_ID = "project-a2ce378b-71f9-4087-95b" 
 REGION = "africa-south1"
 CLUSTER_NAME = "mapungubwe-cluster" 
-COMPOSER_BUCKET = "africa-south1-mapungubwe-co-5f917e09-bucket" 
+COMPOSER_BUCKET = "africa-south1-mapungubwe-co-1780652a-bucket" 
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/MR-Mapungubwe_Park_Records/ingestion/mapungubwe_mysqlToLanding.py"
 PYSPARK_JOB_1 = {"reference": {"project_id": PROJECT_ID}, "placement":{"cluster_name":CLUSTER_NAME}, "pyspark_job":{"main_python_file_uri":GCS_JOB_FILE_1}}
